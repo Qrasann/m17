6386ea4acb7cda75e96353b6db36f305da41acbb
RomaQrasan <68272489+Qrasann@users.noreply.github.com>
2024-12-12 18:38:04 +0300

    Delete less directory

--- less/app/backend/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase  # New
-
-engine = create_engine('sqlite:///ecommerce.db', echo=True)
-SessionLocal = sessionmaker(bind=engine)
-
-
-class Base(DeclarativeBase):  # New
-    pass
